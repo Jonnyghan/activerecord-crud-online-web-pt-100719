3c7d889bfa99c49d2682508fb7dabd52af3468e0
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-01-25 17:26:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,9 +29,9 @@ def can_be_created_in_a_block(args =(title = "Home Alone", release_date = "1990"
   # title == "Home Alone"
   # release_date == 1990
   Movie.create do |m|
-  binding.pry
     m.title = title
     m.release_date = release_date
+  binding.pry
   end
 end
 
